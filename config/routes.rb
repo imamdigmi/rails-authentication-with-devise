876c06eca5275d3b08f65bfb5e2d17dc7670be2a
imam digmi <imam.digmi@gmail.com>
2016-05-10 15:09:07 -0400

    Authorizing access

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   root to: 'pages#index'
+  get '/rahasia', to: 'pages#rahasia', as: :rahasia
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
