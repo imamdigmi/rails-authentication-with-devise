10f56759503d3cb412e7298854d216d2712acae6
imam digmi <imam.digmi@gmail.com>
2016-05-10 15:36:46 -0400

    Modify style

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,15 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :trackable,
-         :validatable,
-         :confirmable,
-         :lockable,
-         :zxcvbnable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :zxcvbnable
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
