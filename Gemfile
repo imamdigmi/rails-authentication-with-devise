f5b3d0f3443780e834db77e920792c47e765e71b
imam digmi <imam.digmi@gmail.com>
2016-05-10 15:03:18 -0400

    Send email and add gem delayed_job

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '3.4.1'
 gem 'bootstrap-sass'
 gem 'jquery-turbolinks'
+gem 'delayed_job_active_record'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'contoh.com',
+    user_name: 'imam.digmi@gmail.com',
+    password: '+_~!Digmi000',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
